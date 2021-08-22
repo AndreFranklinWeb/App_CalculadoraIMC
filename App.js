003efd46cb025b4503a06705679be168e484650e
AndreFranklinWeb <andrefranklinweb@gmail.com>
2021-08-21 23:14:02 -0300

    Primeiro Commit

---
+++ App.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+
+  function handleSubmit() {
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if (imc < 18, 6) {
+      alert('Você está abaixo do peso! ' + imc.toFixed(2));
+    } else if (imc >= 18, 6 && imc < 24.9) {
+      alert('Você está com Peso ideal! ' + imc.toFixed(2));
+    } else if (imc >= 24.9 && imc < 34.9) {
+      alert('Você está levemente acima do peso! ' + imc.toFixed(2));
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Calcule seu IMC</Text>
+
+      <TextInput style={styles.input}
+        value={peso}
+        onChangeText={(peso) => setPeso(peso)}
+        placeholder="Peso (kg)"
+        keyboardType="numeric"
+      />
+
+      <TextInput style={styles.input}
+        value={altura}
+        onChangeText={(altura) => setAltura(altura)}
+        placeholder="Altura (cm)"
+        keyboardType="numeric"
+      />
+
+      <TouchableOpacity style={styles.botao} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Calcular</Text>
+      </TouchableOpacity>
+
+      <Text>Desenvolvido por @anndrefranklin</Text>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  titulo: {
+    textAlign: 'center',
+    marginTop: 25,
+    fontSize: 30
+  },
+
+  input: {
+    backgroundColor: '#121212',
+    borderRadius: 10,
+    margin: 15,
+    padding: 15,
+    color: '#FFF',
+    fontSize: 20,
+  },
+  botao: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+    backgroundColor: '#41AeFa',
+    padding: 10,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 25,
+  }
+
+});
